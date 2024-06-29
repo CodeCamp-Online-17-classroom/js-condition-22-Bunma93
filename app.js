1d60fff27c87355ac418b066f82d762cc4752583
Bunma93 <bunma93@gmail.com>
2024-06-29 12:19:48 +0700

    Js-condition22

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 // write code here
+if (-1 || 0) alert('first');//-1(T) ทำงาน
+if (-1 && 0) alert('second');//0(F) ไม่ทำงาน
+if (null || (-1 && 1)) alert('third');// -1(T) and 1(T) คืนค่า 1(T) ออกมา ทำงาน
